efea5c987b08a4da7246f73dc37e00abfd76e6e0
SerafinPL <57958607+SerafinPL@users.noreply.github.com>
2019-11-23 18:49:03 +0100

    reorganizacja folderów i linków

---
+++ js/head.js
@@ -0,0 +1,2 @@
+
+document.write('"<div id="header-block">  <h1>Web site to iprove the web skills</h1> <div id="menuBox"> <i id="kres1" class="kres"></i> <i id="kres2" class="kres"></i> <i id="kres3" class="kres"></i> </div><!-- id="menuBox" --> </div><!-- id="header-block" -->"');
